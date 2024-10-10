b0c63a43b5535c5532c0501c3a958fe703d14079
Marcus Newton <marcus.newton@hey.com>
2024-10-10 22:19:19 +1100

    update flake

--- flake.nix
+++ flake.nix
@@ -1,26 +1,34 @@
 {
   inputs = {
-    # Remember to update inputs with `nix flake update`
+    # Update inputs with `nix flake update`
+    # Or pin nixpkgs to a specific commit hash
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in with pkgs;
+      let
+        pkgs = import nixpkgs {
+          system = system;
+          config.allowUnfree = true;
+        };
+      in
+      with pkgs;
       {
         devShell = mkShell {
           buildInputs = [
             # search for packages at https://search.nixos.org/
             nixpkgs-fmt
 
-            # (writeShellScriptBin "name" ''
-            # '')
+            (writeShellScriptBin "hello-world" ''
+              echo 'Hello World!'
+            '')
           ];
 
-          # shellHook = ''
-          # '';
+          shellHook = ''
+            echo 'Nix dev environment loaded'
+          '';
         };
       }
     );
